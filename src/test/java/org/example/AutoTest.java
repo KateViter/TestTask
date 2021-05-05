8dea93f50a7323962ab3410a5026c466c209793b
KateViter <83648473+KateViter@users.noreply.github.com>
2021-05-05 09:03:34 +0300

    Final with comments

--- src/test/java/org/example/AutoTest.java
+++ src/test/java/org/example/AutoTest.java
@@ -15,15 +15,33 @@ public class AutoTest {
     @Test
     void moveToYoutube(){
 
-        Configuration.startMaximized=true; //откріть во весь экран
+        // Изменяем настройки браузера чтоб страница была открыта во весь экран
+        Configuration.startMaximized=true;
+
+        // Открываем страницу youtube.com
         open("https://www.youtube.com");
+
+        // Проверяем, что название вкладки - YouTube
         Assertions.assertEquals(Selenide.title(), "YouTube");
+
+        // Создаем переменную которая может принимать за значение рандомное число >=10 и <10000
         int random = ThreadLocalRandom.current().nextInt(10,10000);
+        // На странице находим строку поиска и передаем ей значение random преобразованное в строку
         $("input[id*='search']").sendKeys(String.valueOf(random));
+
+        // Находим на странице кнопку поиска и нажимаем на неё
         $("button[id*='search-icon-legacy']").click();
+
+        // Находим 4 по счету видео в результатах поиска и нажимаем на него
         $("div[id*='contents'] > ytd-video-renderer:nth-child(4)").click();
+
+        // Находим иконку автора видео и нажимаем на неё
         $("div[class*='style-scope ytd-video-secondary-info-renderer'] img").click();
+
+        // Находим кнопку Подписаться и нажимаем на неё
         $("div[id*='channel-header'] div[id*='subscribe-button']").click();
+
+        // Находим кнопку Войти и проверяем, что в кнопке нужный текст - Войти
         $("ytd-modal-with-title-and-button-renderer tp-yt-paper-button").shouldHave(Condition.text("Войти"));
 
     }
